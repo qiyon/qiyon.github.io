27718b3aa2b09e45916f08ba67ed00c91ffa1d77
heqiyong <heqiyon@gmail.com>
2016-03-26 21:07:22 +0800

    route ++

--- src/app/AppRoutes.jsx
+++ src/app/AppRoutes.jsx
@@ -10,7 +10,7 @@ import Master from './components/Master';
 import Home from './components/pages/Home';
 
 const AppRoutes = (
-    <Route path="/" component="{Master}">
+    <Route path="/" component={Master}>
         <IndexRoute component={Home} />
         <Route path="home" component={Home} />
     </Route>
